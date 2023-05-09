b1cf68043cf3da390a0f0ba6b6f151aa2eac1d35
Yunuen AM <yunameza@outlook.com>
2023-05-08 19:12:54 -0600

    ✨Feat:  Navbar with dark style

--- 01-movies-app/src/components/Navbar.jsx
+++ 01-movies-app/src/components/Navbar.jsx
@@ -1,27 +1,27 @@
 const Navbar = () => {
   return (
     <header className='p-3 mb-3 border-bottom'>
-      <div className='container'>
+      <div className='container navbar-expand-lg p-5 bg-dark'>
         <div className='d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start'>
           <a href='/' className='d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none'>
             <svg className='bi me-2' width='40' height='32' role='img' aria-label='Bootstrap'><use xlinkHref='#bootstrap' /></svg>
           </a>
 
           <ul className='nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0'>
-            <li><a href='#' className='nav-link px-2 link-secondary'>Overview</a></li>
+            <li><a href='#' className='nav-link px-2 link-light'>Overview</a></li>
             <li><a href='#' className='nav-link px-2 link-body-emphasis'>List of series</a></li>
 
           </ul>
 
           <form className='col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3' role='search'>
-            <input type='search' className='form-control' placeholder='Search...' aria-label='Search' />
+            <input type='search' className='form-control btn-lg' placeholder='Search...' aria-label='Search' />
           </form>
 
           <div className='dropdown text-end'>
             <a href='#' className='d-block link-dark text-decoration-none dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
               <img src='https://github.com/mdo.png' alt='mdo' width='32' height='32' className='rounded-circle' />
             </a>
-            <ul className='dropdown-menu text-small'>
+            <ul className='dropdown-menu text-large'>
               <li><a className='dropdown-item' href='#'>Home</a></li>
               <li><a className='dropdown-item' href='#'>Profile</a></li>
               <li><a className='dropdown-item' href='#'>Series</a></li>
