f0450975c324c7867c5d46f1f8f4140c2aaec172
Yunuen AM <yunameza@outlook.com>
2023-05-08 20:47:20 -0600

    ✨Feat: images shows API  added

--- 01-movies-app/src/pages/Home.jsx
+++ 01-movies-app/src/pages/Home.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react'
+
 const Home = () => {
+  const [series, setSeries] = useState([]) // lista de series
+  const [searchTerm, setSearchTerm] = useState('') // palabra para buscar
+
+  useEffect(() => {
+    fetch('https://api.tvmaze.com/shows/1/images')
+      .then(response => response.json())
+      .then(data => setSeries(data))
+      .catch(error => console.error(error))
+  }, [])
+
   return (
-    <h1>Home</h1>
+    <div className='container'>
+      <h1>Home</h1>
+    </div>
+
   )
 }
+
 export default Home
