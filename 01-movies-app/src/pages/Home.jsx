284e5fe3f76fa656420a6c0ec47ad2dccb7887cf
Yunuen AM <yunameza@outlook.com>
2023-05-10 12:31:06 -0600

    ✨Feat: searching  function added

--- 01-movies-app/src/pages/Home.jsx
+++ 01-movies-app/src/pages/Home.jsx
@@ -5,31 +5,35 @@ const Home = () => {
   const [series, setSeries] = useState([]) // lista de series
   const [searchTerm, setSearchTerm] = useState('') // palabra para buscar
 
-  const sendSearch = (search) => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
-      .then(response => response.json())
-      .then(data => setSeries(data))
-      .catch(error => console.error(error))
-  }
+  // const sendSearch = (search) => {
+  //   fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+  //     .then(response => response.json())
+  //     .then(data => setSeries(data))
+  //     .catch(error => console.error(error))
+  // }
   useEffect(() => {
-    fetch('https://api.tvmaze.com/shows')
-      .then(response => response.json())
-      .then(data => setSeries(data))
-      .catch(error => console.error(error))
-  }, [])
+    if (searchTerm) {
+      fetch('https://api.tvmaze.com/search/shows?q=' + searchTerm)
+        .then(response => response.json())
+        .then(data => setSeries(data))
+        .catch(error => console.error(error))
+    } else {
+      setSeries([])
+    }
+  }, [searchTerm])
 
   return (
     <div className='container bg-pink rounded-3'>
       <h1>Home</h1>
-      <SearchBar handleSearch={sendSearch} />
+      <SearchBar handleSearch={setSearchTerm} />
       <div className='row'>
         {
             series.map(series => (
-              <div className='col-sm-3 mb-3' key={series.id}>
+              <div className='col-sm-3 mb-3' key={series.show.id}>
                 <div className='card'>
-                  <img className='card-img-top' src={series.image.medium} alt={series.name} />
+                  <img className='card-img-top' src={series.show.image.medium} alt={series.show.name} />
                   <div className='card-body'>
-                    <h5 className='card-title'>{series.name}</h5>
+                    <h5 className='card-title'>{series.show.name}</h5>
                   </div>
                 </div>
               </div>
