23a502fc6913de0cd21945dee5da854b15938246
Yunuen AM <yunameza@outlook.com>
2023-05-10 20:33:40 -0600

    ✨Feat: serieDetail by id added

--- 01-movies-app/src/pages/SerieDetail.jsx
+++ 01-movies-app/src/pages/SerieDetail.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 const SerieDetail = () => {
-  const { id } = useParams().id
+  const { id } = useParams()
   const [serie, setSerie] = useState(null)
+
   useEffect(() => {
-    fetch('https://api.tvmaze.com/search/serie/' + id)
+    fetch(`https://api.tvmaze.com/shows/${id}`)
       .then(response => response.json())
       .then(data => setSerie(data))
       .catch(error => console.error(error))
   }, [id])
+
   if (!serie) {
     return <div>Loading...</div>
   }
   return (
-    <h1>SerieDetail:{serie.show.name}</h1>
+    <h1>Serie detail: {serie.name} </h1>
   )
 }
 export default SerieDetail
