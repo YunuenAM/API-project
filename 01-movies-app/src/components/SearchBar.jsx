94285c4b9d544f1687f4aaa472d3e973cca993ac
Yunuen AM <yunameza@outlook.com>
2023-05-10 18:05:51 -0600

    ✨Feat:  NavLink and to added

--- 01-movies-app/src/components/SearchBar.jsx
+++ 01-movies-app/src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 const SearchBar = ({ handleSearch }) => {
   const [search, setSearch] = useState('')
@@ -6,13 +7,13 @@ const SearchBar = ({ handleSearch }) => {
     <header className='p-3 mb-3 border-bottom'>
       <div className='container navbar-expand-lg p-5 bg-purple rounded-3'>
         <div className='d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start'>
-          <a href='/' className='d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none'>
+          <NavLink to='/' className='d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none'>
             <svg className='bi me-2' width='40' height='32' role='img' aria-label='Bootstrap'><use xlinkHref='#bootstrap' /></svg>
-          </a>
+          </NavLink>
 
           <ul className='nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0'>
-            <li><a href='/Overview' className='nav-link px-2 link-light'>Overview</a></li>
-            <li><a href='/serie' className='nav-link px-2 link-body-emphasis'>Show main information</a></li>
+            <li><NavLink to='/Overview' className='nav-link px-2 link-light'>Overview</NavLink></li>
+            <li><NavLink to='/serie' className='nav-link px-2 link-body-emphasis'>Show main information</NavLink></li>
 
           </ul>
 
@@ -28,17 +29,17 @@ const SearchBar = ({ handleSearch }) => {
           </button>
 
           <div className='dropdown text-end'>
-            <a href='#' className='d-block link-dark text-decoration-none dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
+            <NavLink top='#' className='d-block link-dark text-decoration-none dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
               <img src='https://github.com/YunuenAM.png' alt='mdo' width='94' height='94' className='rounded-circle' />
-            </a>
+            </NavLink>
             <ul className='dropdown-menu text-large'>
-              <li><a className='dropdown-item' href='#'>Home</a></li>
-              <li><a className='dropdown-item' href='#'>Profile</a></li>
-              <li><a className='dropdown-item' href='#'>Series</a></li>
-              <li><a className='dropdown-item' href='#'>Reviews</a></li>
-              <li><a className='dropdown-item' href='#'>Watchlist</a></li>
+              <li><NavLink className='dropdown-item' top='#'>Home</NavLink></li>
+              <li><NavLink className='dropdown-item' top='#'>Profile</NavLink></li>
+              <li><NavLink className='dropdown-item' top='#'>Series</NavLink></li>
+              <li><NavLink className='dropdown-item' top='#'>Reviews</NavLink></li>
+              <li><NavLink className='dropdown-item' top='#'>Watchlist</NavLink></li>
               <li><hr className='dropdown-divider' /></li>
-              <li><a className='dropdown-item' href='#'>Sign out</a></li>
+              <li><NavLink className='dropdown-item' top='#'>Sign out</NavLink></li>
             </ul>
           </div>
         </div>
